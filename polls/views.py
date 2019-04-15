ce6a06648eaa91b9cbf3a3f862b626a667eb874e
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2019-04-15 16:57:47 +0700

    🚩 Week 2 - Database API

--- polls/views.py
+++ polls/views.py
@@ -1,19 +1,19 @@
+import os
+
+from django.db.models import Count
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.db import connection
 
 # Create your views here.
 
-from polls.models import Poll
+from polls.models import Poll, Question
 
 def index(request):
-    # poll_list = [
-    #     {'id': 1, 'title': 'Web Programming'},
-    #     {'id': 2, 'title': 'Web Technology'},
-    #     {'id': 3, 'title': 'Multimedia Technology'},
-    # ]
-    #
+    # load_data_from_sql('polls_preset.sql')
 
-    poll_list = Poll.objects.all()
+    # Get all `Poll` objects, add `question_count` attribute to each poll
+    poll_list = Poll.objects.annotate(question_count=Count('question')).filter(question_count__gt=0)
 
     context = {
         'page_title': 'Welcome to my poll page',
@@ -23,4 +23,18 @@ def index(request):
     return render(request, template_name='polls/index.html', context=context)
 
 def detail(request, poll_id):
-    return HttpResponse('Hello, this is detail page of poll number %i' % poll_id)
+    poll = Poll.objects.get(pk=poll_id)
+
+    context = {
+        'poll': poll
+    }
+
+    return render(request, template_name='polls/detail.html', context=context)
+
+# Non-view functions
+def load_data_from_sql(file_name):
+    file_path = os.path.join(os.path.dirname(__file__), 'sql/', file_name)
+    sql_statement = open(file_path).read()
+
+    with connection.cursor() as cursor:
+        cursor.execute(sql_statement)
