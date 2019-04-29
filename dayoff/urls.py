03e9deec8460b1e1445230b9d588f172f304077c
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2019-04-29 16:59:46 +0700

    :tada: Start new application

---
+++ dayoff/urls.py
@@ -0,0 +1,12 @@
+"""
+    `urls.py`
+    Contains URLs in `polls` application
+"""
+
+from django.urls import path
+
+from dayoff import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
